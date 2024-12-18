1488bd67c6bcc2eecac977bbec2accd8dbfeec5d
Dino0204 <s24020@gsm.hs.kr>
2024-12-18 20:53:47 +0900

    Feat: Card

---
+++ app/components/search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <input
+      type="text"
+      className="border-none outline-none w-2/5 rounded-[36px] p-4"
+    />
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Gamecard from "./components/gamecard";
+import Search from "./components/search";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
+      <Search />
       <section className="flex p-5 gap-4">
         <Gamecard />
         <Gamecard />
